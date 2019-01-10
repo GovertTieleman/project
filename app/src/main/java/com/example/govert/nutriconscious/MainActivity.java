35667a8b40a044bf96bb5f92cec8efa93cd11bd8
Govert Tieleman <govert.tielema@gmail.com>
2019-01-10 11:47:28 +0100

    IC

--- app/src/main/java/com/example/govert/nutriconscious/MainActivity.java
+++ app/src/main/java/com/example/govert/nutriconscious/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.govert.nutriconscious;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView caloriesTextView = findViewById(R.id.textViewCalories);
+
+        caloriesTextView.setText();
+    }
+
+    public void inputClicked(View view) {
+        startActivity(new Intent(MainActivity.this, InputActivity.class));
+    }
+
+    public void planClicked(View view) {
+        startActivity(new Intent(MainActivity.this, PlanActivity.class));
     }
 }
--- app/src/main/java/com/example/govert/nutriconscious/PlanActivity.java
+++ app/src/main/java/com/example/govert/nutriconscious/PlanActivity.java
@@ -4,30 +4,47 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 
-import java.text.Format;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
-public class PlanActivity extends AppCompatActivity {
-    private String ImgId;
+public class PlanActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private String gender, activity, goal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
 
-        // get radioButtons
-        RadioGroup radioGroupActivity = (RadioGroup) findViewById(R.id.radioGroupActivity);
-        RadioGroup radioGroupGoal = (RadioGroup) findViewById(R.id.radioGroupGoal);
+        // get spinner options
+        String[] activityLevels = new String[]{"sedentary", "normal", "active"};
+        String[] goals = new String[]{"loss", "maintain", "gain"};
 
-        // set listener
-        radioGroupActivity.setOnCheckedChangeListener(new OnCheckedChangeListener());
-        radioGroupGoal.setOnCheckedChangeListener(new OnCheckedChangeListener());
+        // make activity level spinner
+        Spinner spinnerActivity = (Spinner) findViewById(R.id.spinnerActivity);
+        ArrayAdapter<String> adapterActivity = new ArrayAdapter<>(PlanActivity.this,
+                R.layout.support_simple_spinner_dropdown_item, activityLevels);
+        adapterActivity.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerActivity.setAdapter(adapterActivity);
 
+        // make goal spinner
+        Spinner spinnerGoals = (Spinner) findViewById(R.id.spinnerGoal);
+        ArrayAdapter<String> adapterGoals = new ArrayAdapter<>(PlanActivity.this,
+                R.layout.support_simple_spinner_dropdown_item, goals);
+        adapterGoals.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerGoals.setAdapter(adapterGoals);
+
+        // set listener for spinner
+        spinnerActivity.setOnItemSelectedListener(this);
+        spinnerGoals.setOnItemSelectedListener(this);
+
+        // get radioGroup
+        RadioGroup radioGroupGender = (RadioGroup) findViewById(R.id.radioGroupGender);
+
+        // set listener for radioGroup
+        radioGroupGender.setOnCheckedChangeListener(new OnCheckedChangeListener());
     }
 
     public void makePlan(View view) {
@@ -45,31 +62,63 @@ public class PlanActivity extends AppCompatActivity {
         User user = new User(null, gender, height, weight, age, activity, goal);
 
         // get db
-        EntryDatabaseHelper db = EntryDatabaseHelper.getInstance(this.getApplicationContext());
+        UserDatabase db = UserDatabase.getInstance(this.getApplicationContext());
 
         // insert into db
-        db.insert(entry);
+        db.insert(user);
 
         // go back to MainActivity
         finish();
         startActivity(new Intent(this, MainActivity.class));
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        int parentId = parent.getId();
+        switch (parentId) {
+            case R.id.spinnerActivity:
+                switch (position) {
+                    case 0:
+                        activity = "sedentary";
+                        break;
+                    case 1:
+                        activity = "normal";
+                        break;
+                    case 2:
+                        activity = "active";
+                        break;
+                }
+                break;
+            case R.id.spinnerGoal:
+                switch (position) {
+                    case 0:
+                        goal = "loss";
+                        break;
+                    case 1:
+                        goal = "maintain";
+                        break;
+                    case 2:
+                        goal = "gain";
+                        break;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // auto generated method stub
+    }
+
     private class OnCheckedChangeListener implements RadioGroup.OnCheckedChangeListener {
         @Override
         public void onCheckedChanged(RadioGroup group, int checkedId) {
             switch (checkedId) {
-                case R.id.radioButton1:
-                    ImgId = "happier";
+                case R.id.male:
+                    gender = "male";
                     break;
-                case R.id.radioButton2:
-                    ImgId = "happy";
-                    break;
-                case R.id.radioButton3:
-                    ImgId = "sad";
-                    break;
-                case R.id.radioButton4:
-                    ImgId = "sadder";
+                case R.id.female:
+                    gender = "female";
                     break;
             }
         }
